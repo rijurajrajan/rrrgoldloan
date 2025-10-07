d7055eb482524227d5e45b33eb7d30161279c74e
RIJU RAJ R <155705388+rijurajrajan@users.noreply.github.com>
2025-10-07 09:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,42 @@ window.addEventListener('scroll', () => {
 topBtn.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+// HERO BACKGROUND SLIDER
+const heroSection = document.querySelector('.hero');
+const heroImages = [
+  'https://images.unsplash.com/photo-1610375461246-83df859d849d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z29sZHxlbnwwfHwwfHx8MA%3D%3D',
+  'https://images.unsplash.com/photo-1662434923031-b9bf1b6c10e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z29sZCUyMGpld2VsbHN8ZW58MHx8MHx8fDA%3D',
+  'https://images.unsplash.com/photo-1710753495848-bf084f9cee54?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Z29sZCUyMGpld2VsbHN8ZW58MHx8MHx8fDA%3D',
+  'https://media.istockphoto.com/id/486592060/photo/indian-jewellery.webp?a=1&b=1&s=612x612&w=0&k=20&c=rC_AgoOUaLzqclvtyNiQsLTs8UtxUOL38myHTD9xrGU='
+];
+
+let currentHeroIndex = 0;
+
+// Set initial image
+heroSection.style.setProperty('--bg-image', `url(${heroImages[0]})`);
+heroSection.style.backgroundImage = `url(${heroImages[0]})`;
+
+// Auto slide every 5 seconds
+setInterval(() => {
+  currentHeroIndex = (currentHeroIndex + 1) % heroImages.length;
+  heroSection.style.backgroundImage = `url(${heroImages[currentHeroIndex]})`;
+}, 5000);
+// 🌄 Background Image Slider for "Our Key Services"
+const featuresSection = document.querySelector('.features');
+const serviceBgImages = [
+  'https://media.istockphoto.com/id/1276740597/photo/indian-traditional-gold-necklace.jpg?s=612x612&w=0&k=20&c=OYp1k0OVJObYq9hqVK_r6NwYa_W54km4nya1R-ovIUY=',
+  'https://media.istockphoto.com/id/486574032/photo/nuptial-chain.jpg?s=612x612&w=0&k=20&c=zy5-WeEK5wDiyZURVULNvBVREmAYDl82UKSWTyIjI-4=',
+  'https://images.unsplash.com/photo-1708251079562-313ec8005354?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGdvbGQlMjBqZXdlbGxzfGVufDB8fDB8fHww',
+  'https://images.unsplash.com/photo-1612813411142-8eb444ff0dff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29sZCUyMGpld2VsbHN8ZW58MHx8MHx8fDA%3D'
+];
+
+let currentServiceBg = 0;
+
+if (featuresSection) {
+  featuresSection.style.backgroundImage = `url(${serviceBgImages[0]})`;
+
+  setInterval(() => {
+    currentServiceBg = (currentServiceBg + 1) % serviceBgImages.length;
+    featuresSection.style.backgroundImage = `url(${serviceBgImages[currentServiceBg]})`;
+  }, 500); // change every 5 seconds
+}
